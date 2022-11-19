a006c66b47af7858185644de29f2ad69ab04def0
Daniel <95552251+d3centra@users.noreply.github.com>
2022-11-20 00:32:57 +0100

    Update Banner.tsx

--- src/components/dashboard/bannerannouncement/Banner.tsx
+++ src/components/dashboard/bannerannouncement/Banner.tsx
@@ -3,6 +3,8 @@ import styled from '@emotion/styled'
 import { Grid, Link } from '@mui/material'
 import { Card, WidgetBody, WidgetContainer } from '../styled'
 import { useRouter } from 'next/router'
+import NextLink from 'next/link'
+import { AppRoutes } from '@/config/routes'
 import { SafeAppsTag } from '@/config/constants'
 import { useRemoteSafeApps } from '@/hooks/safe-apps/useRemoteSafeApps'
 
@@ -26,22 +28,30 @@ export const Banner = (): ReactElement | null => {
   if (!featuredApps?.length && !remoteSafeAppsLoading) return null
 
   return (
-<Grid item xs={12} md>
-  <WidgetContainer id="featured-safe-apps">
-    <WidgetBody>
-      <StyledGrid container>
-        <Card>
-          <Grid container alignItems="center" spacing={3}>
-            <Grid item xs={12} md={9}>
-              <Link color="primary.main" fontWeight="bold" component="span">
-                This is an alternative frontend for Safe.
-              </Link>
-            </Grid>
-          </Grid>
-        </Card>
-      </StyledGrid>
-    </WidgetBody>
-  </WidgetContainer>
-</Grid>;
+    <Grid item xs={12} md>
+      <WidgetContainer id="featured-safe-apps">
+        <WidgetBody>
+          <StyledGrid container>
+            {featuredApps?.map((app) => (
+              <StyledGridItem item xs md key={app.id}>
+                <NextLink passHref href={{ pathname: AppRoutes.apps, query: { ...router.query, appUrl: app.url } }}>
+                  <a>
+                    <Card>
+                      <Grid container alignItems="center" spacing={3}>
+                        <Grid item xs={12} md={9}>
+                          <Link color="primary.main" fontWeight="bold" component="span">
+                            This is an alternative frontend for Safe.
+                          </Link>
+                        </Grid>
+                      </Grid>
+                    </Card>
+                  </a>
+                </NextLink>
+              </StyledGridItem>
+            ))}
+          </StyledGrid>
+        </WidgetBody>
+      </WidgetContainer>
+    </Grid>
   )
 }
